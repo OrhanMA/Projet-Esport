9f2403906e1ff3ce292dc87d312c75e2f133c3f9
OrhanMA <orhan.madi.assani@gmail.com>
2023-04-24 16:58:19 +0200

    made navbar style

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
-<?php $hello = 'Hello, World!' ?>
+
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -7,10 +8,35 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Projet Esport</title>
-  <link rel="stylesheet" href="style.scss">
+  <link rel="stylesheet" href="style.css">
   <script src="script.js"></script>
 </head>
 <body>
-  <h1><?php echo $hello ?></h1>
+  <nav>
+    <div>
+      <button>Players</button>
+      <button>Teams</button>
+      <button>Games</button>
+      <button>Competitions</button>
+    </div>
+  </nav>
+  <form action="./index.php" method="post">
+    <div>
+
+      <div>
+        <label for="firstname">First name</label>
+        <input type="text">
+      </div>
+      <div>
+        <label for="secondname">Second name</label>
+        <input type="text">
+      </div>
+      <div>
+        <label for="city">City</label>
+        <input type="text">
+      </div>
+    </div>
+    <button>Add</button>
+  </form>
 </body>
 </html>
